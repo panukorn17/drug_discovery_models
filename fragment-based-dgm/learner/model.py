4014dfe0c4c35c3910dd5be856abfe0f328b42ef
panukorn17 <p.taleo17@gmail.com>
2022-08-06 22:41:11 +0100

    "auto commit tracked files for new experiment: Molecule Generator on 2022-08-06@22:41:11"

--- fragment-based-dgm/learner/model.py
+++ fragment-based-dgm/learner/model.py
@@ -205,5 +205,5 @@ class Loss(nn.Module):
         # return alpha * CE_loss + (1-alpha) * KL_loss
 
         ### Compute prediction loss
-        pred_loss = 1/len(labels) * torch.sum((pred.type(torch.float64).squeeze(-1) - labels)**2)
+        pred_loss = F.mse_loss(pred.type(torch.float64).squeeze(-1), labels)
         return CE_loss + KL_loss + pred_loss, CE_loss, KL_loss, pred_loss
