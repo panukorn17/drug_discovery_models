1085b31afce73365ffc3568fd79aa2a7ed2d1823
panukorn17 <p.taleo17@gmail.com>
2022-08-08 20:14:16 +0100

    "auto commit tracked files for new experiment: Molecule Generator on 2022-08-08@20:14:16"

--- fragment-based-dgm/learner/trainer.py
+++ fragment-based-dgm/learner/trainer.py
@@ -103,7 +103,8 @@ class Trainer:
         self.vocab = vocab
 
         self.model = Frag2Mol(config, vocab)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.0001)
+        self.optimizer = get_optimizer(config, self.model)
+        #torch.optim.Adam(self.model.parameters(), lr=0.0001)
         self.scheduler = get_scheduler(config, self.optimizer)
         self.criterion = Loss(config, pad=vocab.PAD)
 
