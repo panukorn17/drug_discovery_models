5cc69875edf820b38d622e0cf861068eb31bef31
panukorn17 <p.taleo17@gmail.com>
2022-09-12 18:36:57 +0100

    "auto commit tracked files for new experiment: Molecule Generator on 2022-09-12@18:36:57"

--- fragment-based-dgm/learner/trainer.py
+++ fragment-based-dgm/learner/trainer.py
@@ -153,7 +153,7 @@ class Trainer:
             ### Insert Label
             #print(data_index)
             molecules = dataset.data.iloc[list(data_index)]
-            print("molecules: ", molecules)
+            print("molecules: ", molecules[molecules['fragments'].isin(target_str_ls_2)])
             print("target string list", tgt_str_lst)
             labels = torch.tensor(molecules.logP.values)
             loss, CE_loss, KL_loss, pred_loss = self.criterion(output, tgt, mu, sigma, pred, labels, epoch, tgt_str_lst, penalty_weights)
