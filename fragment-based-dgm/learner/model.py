22cf12acefd530cf5cce5618fe392817c668e60d
panukorn17 <p.taleo17@gmail.com>
2022-09-14 17:15:09 +0100

    "auto commit tracked files for new experiment: Molecule Generator on 2022-09-14@17:15:09"

--- fragment-based-dgm/learner/model.py
+++ fragment-based-dgm/learner/model.py
@@ -253,5 +253,5 @@ class Loss(nn.Module):
         # return alpha * CE_loss + (1-alpha) * KL_loss
 
         ### Compute prediction loss
-        pred_loss = F.BCELoss(pred.type(torch.float64), labels.cuda())
+        pred_loss = F.binary_cross_entropy(pred.type(torch.float64), labels.cuda())
         return CE_loss + KL_loss + pred_loss, CE_loss, KL_loss, pred_loss
