49388622d667c4d0ea689691bd9be64acdda9ed6
panukorn17 <p.taleo17@gmail.com>
2022-09-29 08:37:36 +0100

    "auto commit tracked files for new experiment: Molecule Generator on 2022-09-29@08:37:35"

--- fragment-based-dgm/learner/model.py
+++ fragment-based-dgm/learner/model.py
@@ -99,13 +99,13 @@ class MLP(nn.Module):
             nn.Sigmoid()
         )
         self.layers_logp = nn.Sequential(
-            nn.Linear(latent_size, 64),
+            nn.Linear(latent_size, 200),
             nn.ReLU(),
             #nn.Dropout(0.2),
-            nn.Linear(64, 32),
+            nn.Linear(200, 100),
             nn.ReLU(),
             #nn.Dropout(0.2),
-            nn.Linear(32, 1)
+            nn.Linear(100, 1)
             #nn.Sigmoid()
         )
         self.layers_sas = nn.Sequential(
