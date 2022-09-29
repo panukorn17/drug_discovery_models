9971f3549f39665fcb4e731c65dbcb5b9dab1085
panukorn17 <p.taleo17@gmail.com>
2022-09-29 15:38:00 +0100

    "auto commit tracked files for new experiment: Molecule Generator on 2022-09-29@15:38:00"

--- fragment-based-dgm/learner/model.py
+++ fragment-based-dgm/learner/model.py
@@ -109,13 +109,13 @@ class MLP(nn.Module):
             #nn.Sigmoid()
         )
         self.layers_sas = nn.Sequential(
-            nn.Linear(latent_size, 64),
+            nn.Linear(latent_size, 200),
             nn.ReLU(),
             #nn.Dropout(0.2),
-            nn.Linear(64, 32),
+            nn.Linear(200, 100),
             nn.ReLU(),
             #nn.Dropout(0.2),
-            nn.Linear(32, 1)
+            nn.Linear(100, 1)
             #nn.ReLU(),
             #nn.Dropout(0.2),
             #nn.Sigmoid()
@@ -129,10 +129,10 @@ class MLP(nn.Module):
         #x = self.linear3(x)
         #y_qed = self.layers_qed(x)
         y_logp = self.layers_logp(x)
-        #y_sas = self.layers_sas(x)
+        y_sas = self.layers_sas(x)
         #return x.view(-1)
-        #return y_logp.view(-1).cuda(), y_sas.view(-1).cuda()
-        return y_logp.view(-1).cuda()
+        #return y_logp.view(-1).cuda()
+        return y_logp.view(-1).cuda(), y_sas.view(-1).cuda()
 
 class Decoder(nn.Module):
     def __init__(self, embed_size, latent_size, hidden_size,
@@ -230,8 +230,8 @@ class Frag2Mol(nn.Module):
         #pred_1 = self.mlp(Variable(mu_norm[0, :, :]))
         #pred_2 = self.mlp(Variable(mu_norm[1, :, :]))
         #pred = (pred_1 + pred_2)/2
-        #pred_logp, pred_sas = self.mlp(Variable(mu))
-        pred_logp = self.mlp(Variable(mu))
+        pred_logp, pred_sas = self.mlp(Variable(mu))
+        #pred_logp = self.mlp(Variable(mu))
         ###
         state = self.latent2rnn(z)
         state = state.view(self.hidden_layers, batch_size, self.hidden_size)
@@ -239,8 +239,8 @@ class Frag2Mol(nn.Module):
         output, state = self.decoder(embeddings2, state, lengths)
         #return output, mu, sigma
         ### Teddy Code
-        #return output, mu, sigma, z, pred_logp, pred_sas
-        return output, mu, sigma, z, pred_logp
+        #return output, mu, sigma, z, pred_logp
+        return output, mu, sigma, z, pred_logp, pred_sas
 
     def load_embeddings(self):
         filename = f'emb_{self.embed_size}.dat'
@@ -307,8 +307,8 @@ class Loss(nn.Module):
         self.loss_fn = nn.MSELoss()
         self.vocab = vocab
 
-    def forward(self, output, target, mu, sigma, pred_logp, labels_logp, labels_sas, epoch, tgt_str_lst,penalty_weights, beta):
-        #def forward(self, output, target, mu, sigma, pred_logp, labels_logp, pred_sas, labels_sas, epoch, tgt_str_lst,penalty_weights, beta):
+    def forward(self, output, target, mu, sigma, pred_logp, labels_logp, pred_sas, labels_sas, epoch, tgt_str_lst,penalty_weights, beta):
+        #def forward(self, output, target, mu, sigma, pred_logp, labels_logp, labels_sas, epoch, tgt_str_lst,penalty_weights, beta):
         output = F.log_softmax(output, dim=1)
         #print("molecules logP", labels)
         #print("Original Output Size:", output.size())
@@ -373,5 +373,5 @@ class Loss(nn.Module):
         else:
             #total_loss = CE_loss + beta[epoch]*KL_loss + pred_logp_loss + pred_sas_loss
             total_loss = CE_loss + beta[epoch]*KL_loss + pred_logp_loss
-        #return total_loss, CE_loss, KL_loss, pred_logp_loss, pred_sas_loss
-        return total_loss, CE_loss, KL_loss, pred_logp_loss
+        #return total_loss, CE_loss, KL_loss, pred_logp_loss
+        return total_loss, CE_loss, KL_loss, pred_logp_loss, pred_sas_loss
--- fragment-based-dgm/learner/trainer.py
+++ fragment-based-dgm/learner/trainer.py
@@ -149,8 +149,8 @@ class Trainer:
                 src = src.cuda()
                 tgt = tgt.cuda()
 
-            #output, mu, sigma, z, pred_logp, pred_sas = self.model(src, lengths)
-            output, mu, sigma, z, pred_logp = self.model(src, lengths)
+            output, mu, sigma, z, pred_logp, pred_sas = self.model(src, lengths)
+            #output, mu, sigma, z, pred_logp = self.model(src, lengths)
             #mutual_info = self.model.calc_mi(src, lengths)
             #total_mutual_info += mutual_info * src.size(0)
             #print("mu:", mu)
@@ -167,8 +167,8 @@ class Trainer:
             labels_logp = torch.tensor(molecules_correct.logP.values)
             labels_sas = torch.tensor(molecules_correct.SAS.values)
             #print("labels: ", labels)
-            #loss, CE_loss, KL_loss, pred_logp_loss, pred_sas_loss = self.criterion(output, tgt, mu, sigma, pred_logp, labels_logp, pred_sas, labels_sas, epoch, tgt_str_lst, penalty_weights, beta)
-            loss, CE_loss, KL_loss, pred_logp_loss = self.criterion(output, tgt, mu, sigma, pred_logp, labels_logp, labels_sas, epoch, tgt_str_lst, penalty_weights, beta)
+            loss, CE_loss, KL_loss, pred_logp_loss, pred_sas_loss = self.criterion(output, tgt, mu, sigma, pred_logp, labels_logp, pred_sas, labels_sas, epoch, tgt_str_lst, penalty_weights, beta)
+            #loss, CE_loss, KL_loss, pred_logp_loss = self.criterion(output, tgt, mu, sigma, pred_logp, labels_logp, labels_sas, epoch, tgt_str_lst, penalty_weights, beta)
             #pred_loss.backward()
             loss.backward()
             clip_grad_norm_(self.model.parameters(),
@@ -186,7 +186,7 @@ class Trainer:
                 print("batch ", idx, " loss: ", epoch_loss/(idx+1))
                 #print("pred qed", pred_qed, " labels qed: ", labels_qed, "loss qed:", F.binary_cross_entropy(pred_qed.type(torch.float64), labels_qed.cuda()))
                 print("pred logp", pred_logp, " labels logp: ", labels_logp, "loss logp:", F.mse_loss(pred_logp.type(torch.float64), labels_logp.cuda()))
-                #print("pred sas", pred_sas, " labels sas: ", labels_sas, "loss sas:", F.mse_loss(pred_sas.type(torch.float64), labels_sas.cuda()))
+                print("pred sas", pred_sas, " labels sas: ", labels_sas, "loss sas:", F.mse_loss(pred_sas.type(torch.float64), labels_sas.cuda()))
                 #print("CE Loss ", CE_loss, " KL Loss: ", KL_loss, "Prediction Loss:", pred_logp_loss + pred_sas_loss)
                 print("CE Loss ", CE_loss, " KL Loss: ", KL_loss, "Prediction Loss:", pred_logp_loss)
             ###
